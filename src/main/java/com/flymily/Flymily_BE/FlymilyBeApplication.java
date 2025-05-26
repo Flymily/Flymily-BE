3977d01972230426186fc30b76fca19651deb079
Priscila <pguillen.oliv@gmail.com>
2025-05-26 12:43:33 +0200

    first commit

---
+++ src/main/java/com/flymily/Flymily_BE/FlymilyBeApplication.java
@@ -0,0 +1,13 @@
+package com.flymily.Flymily_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlymilyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlymilyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flymily/Flymily_BE/FlymilyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flymily.Flymily_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlymilyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
