cc8e618aee4f87b7cb185b09f865b7273efae38e
Priscila <pguillen.oliv@gmail.com>
2025-06-02 12:48:30 +0200

    fix java problems with imports

--- src/main/java/com/flymily/flymily/model/Usuario.java
+++ src/main/java/com/flymily/flymily/model/Usuario.java
@@ -1,6 +1,11 @@
 package com.flymily.flymily.model;
 
-import javax.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
@@ -8,7 +13,9 @@ import lombok.*;
 @Data 
 @NoArgsConstructor 
 @AllArgsConstructor 
+
 public class Usuario {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
