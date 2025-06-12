a730d38479a447b0ef2798e6cdaef46acfc63bd9
Priscila <pguillen.oliv@gmail.com>
2025-06-12 12:08:41 +0200

    fix edad ranges, clean code

--- src/main/java/com/flymily/flymily/service/EdadRangoService.java
+++ src/main/java/com/flymily/flymily/service/EdadRangoService.java
@@ -1,10 +1,8 @@
 package com.flymily.flymily.service;
 
 import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
-
 import com.flymily.flymily.model.EdadRango;
 import com.flymily.flymily.repository.EdadRangoRepository;
 
@@ -25,10 +23,7 @@ public class EdadRangoService {
             List.of(
                 new EdadRango(0, 3, "Bebés"),
                 new EdadRango(4, 10, "Niños pequeños"),
-                new EdadRango(11, 17, "Adolescentes"),
-                new EdadRango(18, 120, "Adultos"),
-                new EdadRango(0, 17, "Todos menores"),
-                new EdadRango(4, 120, "Niños y adultos")
+                new EdadRango(11, 17, "Adolescentes")
             ).forEach(edadRangoRepository::save);
         }
     }
