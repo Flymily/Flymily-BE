5d0548492c13586b711c846f14e91c023feae149
Priscila <pguillen.oliv@gmail.com>
2025-06-16 11:33:44 +0200

    create COMUNIDAD entidad

---
+++ src/main/java/com/flymily/flymily/model/Comunidad.java
@@ -0,0 +1,38 @@
+package com.flymily.flymily.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "comunidad")
+@Getter @Setter
+public class Comunidad {
+    
+    @Id
+    @SequenceGenerator (name="comunidad_id_sequence", sequenceName = "comunidad_id_sequence", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "comunidad_id_sequence")
+    private Long id;
+
+    @Column (name="Título")
+    @NotBlank
+    @Size (max = 100, message = "(!) ERROR: el campo del título no puede tener más de 100 caracteres")
+    private String tituloComunidad;
+
+    @Column (name="Contenido")
+    @NotBlank
+    @Size (max = 5000, message = "(!) ERROR: el campo del contenido no puede tener más de 5000 caracteres")
+    private String contenidoComunidad;
+
+    @Column (name="Imagen")
+    private String imgPathComunidad;
+
+}
