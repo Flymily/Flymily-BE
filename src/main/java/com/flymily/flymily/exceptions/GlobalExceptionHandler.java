209bd0a85be8b4410e6a279ad1c296c95cc266f1
Priscila <pguillen.oliv@gmail.com>
2025-06-16 12:51:04 +0200

    add missing exceptions to the global handler

--- src/main/java/com/flymily/flymily/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/flymily/flymily/exceptions/GlobalExceptionHandler.java
@@ -40,4 +40,29 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("(!) ERROR: " + ex.getMessage());
     }
 
+    @ExceptionHandler(EdadRangoNotFoundException.class)
+    public ResponseEntity<String> handleEdadRangoNotFoundException(EdadRangoNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("(!) ERROR: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidFilterException.class)
+    public ResponseEntity<String> handleInvalidFilterException(InvalidFilterException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("(!) ERROR: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(LocalidadNotFoundException.class)
+    public ResponseEntity<String> handleLocalidadNotFoundException(LocalidadNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("(!) ERROR: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(TipoViajeIdNotFoundException.class)
+    public ResponseEntity<String> handleTipoViajeIdNotFoundException(TipoViajeIdNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("(!) ERROR: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(TipoViajeNotFoundException.class)
+    public ResponseEntity<String> handleTipoViajeNotFoundException(TipoViajeNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("(!) ERROR: " + ex.getMessage());
+    }
+
 }
